a2392090cd7e82fc1bfed67a21186f7fdd6ced23
Keshav Thakur <keskumar2001@gmail.com>
2023-11-04 19:40:09 +0530

    modified 404 page

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import "./style.scss"
+import pageNotFoundImg from "../../assets/page-not-found.svg"
 const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
+    <div className='outer-div'>
+      <img src={pageNotFoundImg} alt="vector" />
+      <div className="wrapper">
+        <h1>Page Not Found</h1>
+        <p className="message">Sorry! The page you are looking for doesn't exits, please try to hit right url.</p>
+        <a href="/" className="btn">Learn More About Us</a>
+      </div>
+    </div>
   )
 }
 
